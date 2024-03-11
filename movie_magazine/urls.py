8a78f1801dbf3098c4254769a549a7cf371c9af6
Eugene Protsenko <guitaaarpro@gmail.com>
2024-03-11 17:47:42 +0100

    creating templates and url

---
+++ movie_magazine/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from movie_magazine.views import (index,
+                                  TopicListView,
+                                  TopicCreateView,
+                                  TopicUpdateView,
+                                  TopicDeleteView)
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete")
+]
+
+app_name = "movie_magazine"
