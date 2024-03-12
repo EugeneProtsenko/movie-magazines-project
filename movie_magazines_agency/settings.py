dd51eb911a7128613302b6bd1e86ee33ff3bbc29
Eugene Protsenko <guitaaarpro@gmail.com>
2024-03-12 17:04:10 +0100

    modifing templates

--- movie_magazines_agency/settings.py
+++ movie_magazines_agency/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "movie_magazine.Critic"
+AUTH_USER_MODEL = 'movie_magazine.Critic'
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
