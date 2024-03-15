cf531266d5409b2867d2a668d9b8f9525a9aa2a6
Eugene Protsenko <guitaaarpro@gmail.com>
2024-03-15 19:18:29 +0100

    modifing settings, and changing DB

--- movie_magazines_agency/settings.py
+++ movie_magazines_agency/settings.py
@@ -94,7 +94,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
-DATABASE_URL = "postgres://uuqsoudr:KfFufWQPBUS1O3wLwjKUBIeeu6ZfWKZa@cornelius.db.elephantsql.com/uuqsoudr"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
